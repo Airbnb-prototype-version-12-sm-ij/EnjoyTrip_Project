fafc340ea11a3964d5f70040a4cf536f85042350
SeoungMinHwang <hsm990423@gmail.com>
2024-05-22 15:48:53 +0900

    feat :DB데이터

--- src/main/java/com/ssafy/enjoytrip/domain/review/controller/ReviewController.java
+++ src/main/java/com/ssafy/enjoytrip/domain/review/controller/ReviewController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.enjoytrip.domain.member.entity.MemberEntity;
@@ -29,7 +30,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Controller
+@RestController
 @Slf4j
 @RequiredArgsConstructor
 public class ReviewController {
