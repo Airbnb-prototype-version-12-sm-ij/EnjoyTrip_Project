450ee33c04e6a2dab8bfcf5deef3460fe6a73590
lij0825 <lij0825@gmail.com>
2024-05-22 18:00:35 +0900

    feat : swagger 적용

--- src/main/java/com/ssafy/enjoytrip/domain/review/controller/ReviewController.java
+++ src/main/java/com/ssafy/enjoytrip/domain/review/controller/ReviewController.java
@@ -11,13 +11,13 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.enjoytrip.domain.member.entity.MemberEntity;
@@ -29,7 +29,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Controller
+@RestController
 @Slf4j
 @RequiredArgsConstructor
 public class ReviewController {
